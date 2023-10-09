52aeeed3c7bab6e9867a3b68b07d56a4dcaeb5ac
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-10-09 18:17:47 -0400

    comment out function that call function 3 times with the same arg and call add_up function 3 times with 3 different args

--- add_up.rb
+++ add_up.rb
@@ -10,22 +10,28 @@ end
 
 # call the function 3 times using while loop
 
-def print_three_times(number)
-  times = 0
-  while times < 3
-    puts add_up(number)
-    times += 1
-  end
-end
+# def print_three_times(number)
+#   times = 0
+#   while times < 3
+#     puts add_up(number)
+#     times += 1
+#   end
+# end
 
-print_three_times(3)
+# print_three_times(3)
 
-# call the function 3 times using times iteration
+# # call the function 3 times using times iteration
 
-def print_three_times_v2(number)
-  3.times do
-    puts add_up(number)
-  end
-end
+# def print_three_times_v2(number)
+#   3.times do
+#     puts add_up(number)
+#   end
+# end
 
-print_three_times_v2(3)
+# print_three_times_v2(3)
+
+# # call the function 3 times
+puts add_up(3)
+puts add_up(6)
+puts add_up(9)
+
